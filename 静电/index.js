dfb123970c7cf3bf45ae31dd42816c404b8f78ac
3045395783 <109497460+3045395783@users.noreply.github.com>
2022-07-18 13:41:57 +0800

    Update static/index.js

--- 静电/index.js
+++ 静电/index.js
@@ -343,11 +343,11 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 30) return '鸡你太美';
-    if (score <= 60) return '鸡你太太美';
-    if (score <= 100) return '鸡鸡鸡鸡鸡';
-    if (score <= 150) return '唱 跳 rap 篮球';
-    return '你就是蔡徐坤本人？';
+    if (score <= 30) return '小辣鸡';
+    if (score <= 60) return '大辣鸡';
+    if (score <= 100) return '辣鸡pro';
+    if (score <= 150) return '辣鸡pro max';
+    return '老八都吃不过你';
 }
 
 function toStr(obj) {
